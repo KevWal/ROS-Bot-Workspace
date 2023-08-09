c7951976281fffe293ab07ea367ede3d0e3e6806
Kevin Walton <kevin@unseen.org>
2023-08-09 23:02:19 +0100

    Updates

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-# Turn USB port power on
-sudo uhubctl --action on -l 2
-
-sleep 6
+if uhubctl | grep -q "Port 2: 0080 off" ;
+then
+	echo "Turning USB port power on"
+	sudo uhubctl --action on -l 2 > /dev/null
+	sleep 6
+else
+	echo "USB Power already on"
+fi
 
 roslaunch edukit_bot bringup.launch
